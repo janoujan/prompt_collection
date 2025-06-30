8265402eae188ca561a169b1409d82b28f1c4e8a
janoujan <janou@janou.pro>
2025-06-30 16:55:28 +0200

    fix header.php

--- header.php
+++ header.php
@@ -7,6 +7,7 @@ echo '
     <li><a href="index.php" ' . ($page === 'index.php' ? 'class="active"' : '') . '>Accueil</a></li>
     <li><a href="ajout_prompt.php" ' . ($page === 'ajout_prompt.php' ? 'class="active"' : '') . '>Ajouter un prompt</a></li>
     <li><a href="liste_prompts.php" ' . ($page === 'liste_prompts.php' ? 'class="active"' : '') . '>Liste des prompts</a></li>
+    <li><a href="contact.php" ' . ($page === 'contact.php' ? 'class="active"': '') . '>Contact</a></li>
     <li><a href="about.php" ' . ($page === 'about.php' ? 'class="active"' : '') . '>À propos</a></li>
   </ul>
 </nav>';
