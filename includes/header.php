c1800c53299df34a25d1354b355bdd0589d1f6b3
janoujan <janou@janou.pro>
2025-07-01 15:34:06 +0200

    add some styles

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@ $script_name = basename($_SERVER['SCRIPT_NAME']);
 
 echo '
 <nav>
-  <ul>
-    <li><a href="' . BASE_URL . '/index.php" ' . ($script_name === 'index.php' ? 'class="active"' : '') . '><img src="' . BASE_URL . '/assets/images/logo_prompt_collection.png" alt="logo" width="120px" height="120px"></a></li>
+  <ul class="header">
+    <li><a href="' . BASE_URL . '/index.php" ' . ($script_name === 'index.php' ? 'class="active"' : '') . '><img src="' . BASE_URL . '/assets/images/logo_prompt_collection.png" alt="logo" class="logo" width="120px" height="120px"></a></li>
     <li><a href="' . BASE_URL . '/index.php" ' . ($script_name === 'index.php' ? 'class="active"' : '') . '>Accueil</a></li>
     <li><a href="' . BASE_URL . '/prompt/ajout_prompt.php" ' . ($script_name === 'ajout_prompt.php' ? 'class="active"' : '') . '>Ajouter un prompt</a></li>
     <li><a href="' . BASE_URL . '/prompt/liste_prompts.php" ' . ($script_name === 'liste_prompts.php' ? 'class="active"' : '') . '>Liste des prompts</a></li>
